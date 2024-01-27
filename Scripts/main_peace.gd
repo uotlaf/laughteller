6fd1afa035538f0f8a6eec711cf574a51b3d355e
uotlaf <uotlaf@protonmail.com>
2024-01-27 00:51:45 -0300

    Script de hold do mouse refatorado

--- Scripts/main_peace.gd
+++ Scripts/main_peace.gd
@@ -2,16 +2,12 @@ extends CharacterBody2D
 
 class_name Main_Peace
 
-var mouse_on: bool = false
+var hold: bool = false
 
 func _physics_process(_delta: float):
-	if Input.is_action_pressed("ui_left_mouse"):
-		if mouse_on:
-			global_position = get_global_mouse_position()
+	if hold:
+		global_position = get_global_mouse_position()
 
-
-func mouse_ent():
-	mouse_on = true
-
-func mouse_ext():
-	mouse_on = false
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		hold = event.is_pressed()
