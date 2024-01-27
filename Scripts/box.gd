71743c5b6c9107e95bdcfdfdbeea94fd046667c9
Maxwell <maxwellpires2002@gmail.com>
2024-01-27 14:52:42 -0300

    Grab and Ungrab Finalized

--- Scripts/box.gd
+++ Scripts/box.gd
@@ -4,24 +4,21 @@ class_name Box
 var has_grabbed: bool = false
 
 @export var number_box : int = 1
-#ar object : CharacterBody2D # Bloco que está sendo segurado
+var object : CharacterBody2D # Bloco que está sendo segurado
 
 func _on_area_2d_body_entered(body):
 	if body is Main_Peace:
 		if body.box_turn and not has_grabbed:
-			body.grab(self)
 			has_grabbed = true
-
+			object = body
+			body.grab(self)
+			
 	# Corpo está sendo segurado pelo cursor e não tem um expandido
-	#if body is Main_Peace and body.hold and not object and not has_grabbed:
-		#object = body
-		#has_grabbed = true
-		#object.grab(self)
-
+	
 func _on_area_2d_body_exited(body):
 	# Corpo está sendo segurado pelo cursor e é o que está sendo segurado pela caixa
-	if body is Main_Peace: #and body.hold and body == object:
+	if body is Main_Peace and body.hold and body == object:
 		has_grabbed = false
 		body.ungrab()
-		#object.ungrab()
-		#object = null
+		object = null
+		
--- Scripts/management.gd
+++ Scripts/management.gd
@@ -6,7 +6,7 @@ const jokes: Dictionary = {
 	"1": {
 		"nome": "Primeira Piada",
 		"p1": {
-			"path_p": "res://Assets/icon.svg",
+			"path_p": "res://Assets/cursor.png",
 			"path_b1": "res://Assets/Moldura_vazia.png",
 			"path_b2": "res://Assets/icon.svg",
 			"path_b3": "res://Assets/cursor.png"
