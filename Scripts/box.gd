dbc1ab47491f1fcc49fa137ad3c7740d331a891d
Maxwell <maxwellpires2002@gmail.com>
2024-01-26 23:07:30 -0300

    Grab and Ungrab Movement

--- Scripts/box.gd
+++ Scripts/box.gd
@@ -6,3 +6,8 @@ class_name Box
 func _on_area_2d_body_entered(body):
 	if body is Main_Peace:
 		body.grab(self)
+
+
+func _on_area_2d_body_exited(body):
+	if body is Main_Peace:
+		body.ungrab()
--- Scripts/main_peace.gd
+++ Scripts/main_peace.gd
@@ -2,15 +2,26 @@ extends CharacterBody2D
 
 class_name Main_Peace
 
+var in_box = false
 var mouse_on: bool = false
+var original_position: Vector2
 
+
+	
 func _physics_process(_delta: float):
+	if not in_box:
+		global_position = original_position
 	if Input.is_action_pressed("ui_left_mouse"):
 		if mouse_on:
 			global_position = get_global_mouse_position()
-
+		
+			
 func grab(box: Box):
+	in_box = true
 	global_position = box.global_position
+
+func ungrab():
+	in_box = false
 	
 func mouse_ent():
 	mouse_on = true
