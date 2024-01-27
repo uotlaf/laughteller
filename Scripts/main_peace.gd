573f3422555b6c1cabdbd434f2bb7a479ccd8d75
Maxwell <maxwellpires2002@gmail.com>
2024-01-27 12:07:19 -0300

    Twwen criado para Box

--- Scripts/main_peace.gd
+++ Scripts/main_peace.gd
@@ -8,7 +8,7 @@ var in_box = false
 var original_position: Vector2 
 var hold: bool = false
 var Box_realtive: Box
-
+var tween_finish = false
 
 
 func _ready():
@@ -17,15 +17,23 @@ func _ready():
 	$BoxImage.scale = Vector2(2.75, 2.75)
 	update_textures($Initial/Sprite2D, "p", number_peace)
 	
-	
 func _physics_process(_delta: float):
+	
 	if not in_box:
-		global_position = original_position
-		$BoxImage.visible = false
-		$Initial.visible = true
-	else:
-		global_position = Box_realtive.global_position
 		if not hold:
+			var my_tween = get_tree().create_tween()
+			my_tween.finished.connect(finished_tween)
+			my_tween.tween_property(self, "position", original_position, 0.25).set_trans(Tween.TRANS_LINEAR)
+			#global_position = original_position
+			$BoxImage.visible = false
+			$Initial.visible = true
+	else:
+		if not hold:
+			#global_position = Box_realtive.global_position
+			var my_tween = get_tree().create_tween()
+			tween_finish = false
+			my_tween.finished.connect(finished_tween)
+			my_tween.tween_property(self, "position", Box_realtive.global_position, 0.25).set_trans(Tween.TRANS_LINEAR)
 			$Initial.visible = false
 			$BoxImage.visible = true
 		else:
@@ -35,7 +43,7 @@ func _physics_process(_delta: float):
 		global_position = get_global_mouse_position()
 	
 		
-			
+	
 func grab(box: Box):
 	Box_realtive = box
 	update_textures($BoxImage, "b", Box_realtive.number_box)
@@ -51,7 +59,6 @@ func _on_area_2d_input_event(_viewport, event, _shape_idx):
 
 func update_textures(sprite: Sprite2D, type: String, number_index: int):
 	var image = Image.new()
-	print(type+str(number_index))
 	if type == "p":
 		image.load(Joke.jokes[str(Joke.act_joke)][type+str(number_peace)]["path_"+type])
 	if type == "b":
@@ -60,4 +67,8 @@ func update_textures(sprite: Sprite2D, type: String, number_index: int):
 	var t = ImageTexture.new()
 	t = ImageTexture.create_from_image(image)
 	sprite.texture = t
+
+func finished_tween():
+	tween_finish = true
+
 	
