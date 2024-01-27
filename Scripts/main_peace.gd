031bf20f22d301aca2ca234e5a8bbe81746e6f70
Maxwell <maxwellpires2002@gmail.com>
2024-01-27 02:23:03 -0300

    Image Dynamic

--- Scripts/main_peace.gd
+++ Scripts/main_peace.gd
@@ -2,21 +2,30 @@ extends CharacterBody2D
 
 class_name Main_Peace
 
+var n_joke: int = 0
 var in_box = false
 var original_position: Vector2 
 var hold: bool = false
 var Box_realtive: Box
-	
+
+
+
+func _ready():
+	$BoxImage.texture = preload("res://Assets/Moldura_vazia.png")
+	$BoxImage.scale = Vector2(2.75, 2.75)
 func _physics_process(_delta: float):
 	if not in_box:
 		global_position = original_position
 		$BoxImage.visible = false
 		
+		
 	else:
 		global_position = Box_realtive.global_position
 		if not hold:
 			$BoxImage.visible = true
+			$Sprite2D.modulate = Color("101010")
 		else:
+			$Sprite2D.modulate = Color("FFFFFF")
 			$BoxImage.visible = false
 	if hold:
 		global_position = get_global_mouse_position()
@@ -30,6 +39,6 @@ func grab(box: Box):
 func ungrab():
 	in_box = false
 	
-func _on_area_2d_input_event(viewport, event, shape_idx):
+func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		hold = event.is_pressed()
--- Scripts/test_spawner.gd
+++
@@ -1,8 +0,0 @@
-extends Marker2D
-
-const PEACE: PackedScene = preload("res://Scenes/test_outrapeace.tscn")
-
-func _ready():
-	var peace = PEACE.instantiate()
-	peace.original_position = global_position
-	get_parent().call_deferred("add_child", peace)
