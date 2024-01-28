08e19d3b94f791414c6fc13f7518e259cd53ecd3
Andreirl032 <andrei.ramos.lopes@gmail.com>
2024-01-28 02:09:01 -0300

    atualizada funcionalidade de mudança de idioma

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,28 +1,22 @@
 extends Control
 
-@onready var new_game_button:Button = get_node("MarginContainer/VBoxContainer/NewGame");
-@onready var options_button:Button = get_node("MarginContainer/VBoxContainer/Options");
-@onready var quit_button:Button = get_node("MarginContainer/VBoxContainer/Quit");
+@onready var new_game_button:Button = get_node("MarginContainer/VBoxContainer/NewGame")
+@onready var options_button:Button = get_node("MarginContainer/VBoxContainer/Options")
+@onready var quit_button:Button = get_node("MarginContainer/VBoxContainer/Quit")
+
 
 func _ready():
-	if(Management.current_language==0):
-		new_game_button.text = "Jogar"
-		options_button.text = "Opções"
-		quit_button.text = "Sair"
-		pass;
-		
-	elif(Management.current_language==1):
-		new_game_button.text = "New game"
-		options_button.text = "Options"
-		quit_button.text = "Quit"
-		pass;
+	var text_data = Management.data.text
+	new_game_button.text = text_data.play
+	options_button.text = text_data.options
+	quit_button.text = text_data.quit
 
 func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://Scenes/test.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn");
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 
 func _on_quit_pressed():
-	get_tree().quit();
+	get_tree().quit()
 	
--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -1,22 +1,27 @@
 extends Control
 
-@onready var button_pt:TextureButton = get_node("Languages/Portugues");
-@onready var button_en:TextureButton = get_node("Languages/English");
+@onready var button_pt:TextureButton = get_node("Languages/Portugues")
+@onready var button_en:TextureButton = get_node("Languages/English")
+
+@onready var button_back:Button = get_node("MarginContainer/VBoxContainer/Back")
 
 func _ready():
-	prints(button_pt);
 	if(Management.current_language==0):
-		button_pt.button_pressed=true;
+		button_pt.button_pressed=true
 	elif(Management.current_language==1):
-		button_en.button_pressed=true;
+		button_en.button_pressed=true
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn");
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 
 func _on_portugues_toggled(toggled_on):
-	Management.load_language(0);
-	print(Management.current_language);
+	Management.load_language(0)
+	update_option_texts()
 
 func _on_english_toggled(toggled_on):
-	Management.load_language(1);
-	print(Management.current_language);
+	Management.load_language(1)
+	update_option_texts()
+
+func update_option_texts():
+	button_back.text = Management.data.text.back
+	
