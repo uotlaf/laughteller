46e230efcb3d8b165e286701282c5c41e43efbc0
uotlaf <uotlaf@protonmail.com>
2024-01-28 12:53:20 -0300

    stage select now change to main_scene

--- Scripts/peace_select_button.gd
+++ Scripts/peace_select_button.gd
@@ -6,4 +6,4 @@ var chapter_id
 func on_pressed():
 	Management.selected_chapter = str(chapter_id)
 	Management.selected_joke = str(button_id)
-	get_tree().change_scene_to_file("res://Scenes/test.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
