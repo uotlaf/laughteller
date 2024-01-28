b40a0efaf2db67e41f5998f2a04b019a15bf8d89
uotlaf <uotlaf@protonmail.com>
2024-01-28 11:24:28 -0300

    Saveguard cast for peace_select_button

--- Scripts/peace_select_button.gd
+++ Scripts/peace_select_button.gd
@@ -4,6 +4,6 @@ var button_id
 var chapter_id
 
 func on_pressed():
-	Management.selected_chapter = chapter_id
-	Management.selected_joke = button_id
-	get_tree().change_scene_to_file("res://Scenes/main_peace.tscn")
+	Management.selected_chapter = str(chapter_id)
+	Management.selected_joke = str(button_id)
+	get_tree().change_scene_to_file("res://Scenes/test.tscn")
