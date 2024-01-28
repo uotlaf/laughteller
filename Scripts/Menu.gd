f0d27447aa1547db7e0cdf6c93fe2b91a789315a
Andreirl032 <andrei.ramos.lopes@gmail.com>
2024-01-28 14:41:13 -0300

    alterados botões do menu

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,17 +1,16 @@
 extends Control
 
-@onready var new_game_button:Button = get_node("MarginContainer/VBoxContainer/NewGame")
+@onready var play_button:Button = get_node("MarginContainer/VBoxContainer/Play")
 @onready var options_button:Button = get_node("MarginContainer/VBoxContainer/Options")
 @onready var quit_button:Button = get_node("MarginContainer/VBoxContainer/Quit")
 
-
 func _ready():
 	var text_data = Management.data.text
-	new_game_button.text = text_data.play
+	play_button.text = text_data.play
 	options_button.text = text_data.options
 	quit_button.text = text_data.quit
 
-func _on_new_game_pressed():
+func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/peace_select.tscn")
 
 func _on_options_pressed():
