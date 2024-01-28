357aed99fc557fcbb42eb875afce8603cc2a4eb4
uotlaf <uotlaf@protonmail.com>
2024-01-28 00:34:08 -0300

    new json and language system

--- Scripts/management.gd
+++ Scripts/management.gd
@@ -4,17 +4,26 @@ var act_joke = 0
 
 var current_language : bool = 0
 
-const jokes_json_path : String = "res://Assets/Jokes/info.json"
+var languages : Array = ["pt", "en"]
 
-var jokes: Dictionary 
+const jokes_json_path : String = "res://Assets/Jokes/"
 
-func _ready():
-	var file = FileAccess.open(jokes_json_path, FileAccess.READ)
+var data: Dictionary 
+
+func load_language(language_no):
+	var path : String = jokes_json_path + "info_" + languages[int(current_language)] + ".json"
+	var file = FileAccess.open(path, FileAccess.READ)
 	assert(file)
 	var json = JSON.new()
 	assert(json.parse(file.get_as_text()) == OK)
-	jokes = json.data
+	data = json.data
+	
 
+# Carrega as piadas
+func _ready():
+	load_language(0)
+
+# Tela cheia
 func _unhandled_input(event):
 	if event is InputEventKey and event.get_keycode() == KEY_F11 and event.is_pressed():
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
